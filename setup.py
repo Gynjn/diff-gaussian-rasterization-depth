21d1b29ad6e8db5f1c3891670e36c050300ed8f4
Gynjn <ggggjin99@gmail.com>
2024-09-28 10:32:16 +0000

    conf_mode

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization_depth",
-    packages=['diff_gaussian_rasterization_depth'],
+    name="diff_gaussian_rasterization_depth_conf",
+    packages=['diff_gaussian_rasterization_depth_conf'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization_depth._C",
+            name="diff_gaussian_rasterization_depth_conf._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
